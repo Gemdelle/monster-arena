9e2c98523f49eaa2bfa2f612bddb75a14a5c361c
Gemdelle <milu200911@hotmail.com>
2023-10-30 18:46:51 -0300

    jumping.

--- final.py
+++ final.py
@@ -14,7 +14,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 # Surfaces
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('Monster Arena', False, 'Black') # text, antianaliser, color.
+
+score_surf = test_font.render('Monster Arena', False, (64,64,64)) # text, antianaliser, color.
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (600,300))
@@ -27,20 +29,48 @@ while True: # The game will be continuously updated.
         if event.type == pygame.QUIT:
             pygame.quit() 
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('collision')
+        #     else:
+        #         print('no collision')
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                    print('jump')
+        if event.type == pygame.KEYUP:
+            print('key up')
 
     screen.blit(sky_surface,(0,0)) # block image transfer -> to display images in screen
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
+
+    pygame.draw.rect(screen,'#c0e8ec',score_rect)
+    pygame.draw.rect(screen,'#c0e8ec',score_rect,12)
+    screen.blit(score_surf,score_rect)
+
     screen.blit(snail_surface,snail_rect)
     snail_rect.x -= 3
     if snail_rect.left <= -50:
         snail_rect.right = 850
     screen.blit(player_surface,player_rect)
 
-    if player_rect.colliderect(snail_rect):
-        print('collision')
-    else:
-        print('Free')
+    # Keys
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print('jump')
+
+    # if player_rect.colliderect(snail_rect):
+    #     print('collision')
+    # else:
+    #     print('Free')
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print('collision')
+    # else:
+    #     print('no collision')
 
     pygame.display.update() # update the screen
     clock.tick(60) # while True runs 60 times per second
+
+    # Extra things -----------------------------------------------------------------------------------------------
+    #     pygame.draw.line(screen,'Black',(0,0),pygame.mouse.get_pos(),2)
