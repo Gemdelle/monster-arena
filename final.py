7a98dd0647011f8fe92b28e7d5faf16e0178f32e
Gemdelle <milu200911@hotmail.com>
2023-10-30 14:49:25 -0300

    while True.

--- final.py
+++ final.py
@@ -1,4 +1,11 @@
 import pygame
+from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,400)) # Create screen
+screen = pygame.display.set_mode((800,400)) # Create screen. This code ends, so to keep it running we use the while True (is never False).
+
+while True: # The game will be continuously updated.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() 
+    pygame.display.update() # update the screen
