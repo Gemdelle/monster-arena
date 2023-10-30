96334fb1a07a843aa5703a63ed659ee296ee0217
Gemdelle <milu200911@hotmail.com>
2023-10-30 18:01:30 -0300

    collisions.

--- final.py
+++ final.py
@@ -12,12 +12,15 @@ clock = pygame.time.Clock() # clock object to handle frame rate
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 # Surfaces
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('Monster Arena', False, 'Black') # text, antianaliser, color.
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_position = 600
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600,300))
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(bottomright = (600,300))
 
 while True: # The game will be continuously updated.
     for event in pygame.event.get():
@@ -28,11 +31,16 @@ while True: # The game will be continuously updated.
     screen.blit(sky_surface,(0,0)) # block image transfer -> to display images in screen
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    screen.blit(snail_surface,(snail_x_position,265))
+    screen.blit(snail_surface,snail_rect)
+    snail_rect.x -= 3
+    if snail_rect.left <= -50:
+        snail_rect.right = 850
+    screen.blit(player_surface,player_rect)
 
-    snail_x_position -= 4
-    if snail_x_position < -50:
-        snail_x_position = 850
+    if player_rect.colliderect(snail_rect):
+        print('collision')
+    else:
+        print('Free')
 
     pygame.display.update() # update the screen
     clock.tick(60) # while True runs 60 times per second
