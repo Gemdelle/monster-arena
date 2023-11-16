9dea7919523dbd8efc62cf128c8be76568bc8016
Gemdelle <milu200911@hotmail.com>
2023-11-16 16:48:39 -0300

    sky-backgrounds.

--- final.py
+++ final.py
@@ -464,7 +464,7 @@ def play_lose_sound():
     lose_sound.play()
 
 def play_background_music():
-    background_music = pygame.mixer.Sound('audio/music.mp3')
+    background_music = pygame.mixer.Sound('audio/intro-music.mp3')
     background_music.set_volume(0.2)
     background_music.play()
 
